bad228c78ba6f174a20bae777f56af64811e05d9
wooooseok <pws6882586@naver.com>
2022-06-13 17:46:00 +0900

    change~!

--- src/com/kh/git/HelloGit.java
+++ src/com/kh/git/HelloGit.java
@@ -1,8 +1,14 @@
 package com.kh.git;
 
+import com.kh.git.animal.Dog;
+
 public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("HelloGit!!!");
+		
+		// Dog 기능추가
+		Dog dog = new Dog();
+		dog.bark();
 	}
 }
--- src/com/kh/git/animal/Dog.java
+++ src/com/kh/git/animal/Dog.java
@@ -2,4 +2,9 @@ package com.kh.git.animal;
 
 public class Dog {
 
+	public void bark() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
